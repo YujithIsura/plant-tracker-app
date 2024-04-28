fb0670754db91b7fb1445afefb9cf957af7c047e
YujithIsura <ujithisura@gmail.com>
2024-04-28 22:43:44 +0530

    lint

---
+++ plant-tracker-front-end-node/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve Flutter project from the public-flutter directory
+app.use(express.static(path.join(__dirname, 'public-flutter')));
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
