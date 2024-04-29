e889cd9f7450e8c2bae7279c246e9b31de10f9ca
YujithIsura <ujithisura@gmail.com>
2024-04-29 11:25:22 +0530

    port changed

--- plant-tracker-front-end-node/index.js
+++ plant-tracker-front-end-node/index.js
@@ -8,7 +8,9 @@ app.use(express.static(path.join(__dirname, 'public-flutter')));
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 8080;
+const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
